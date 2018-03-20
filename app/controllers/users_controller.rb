0940c42d52547ce9efa689126b787f2f5137c9f8
Penguino <heriberto@melo.nyc>
2018-03-20 17:37:27 -0400

    Created Signup Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,21 @@
 class UserController < ApplicationController
+  get '/signup' do
+    if logged_in?
+      redirect '/recipes'
+    else
+      erb :'/users/signup'
+    end
+  end
+
+  post '/signup' do
+    if User.find_by(username: params[:username])
+      #flash username taken
+      redirect '/signup'
+    else
+      @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect to "/#{@user.slug}"
+    end
+  end
 
 end
