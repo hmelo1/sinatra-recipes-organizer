b0984f85eddc81b6c6d5ed72f05ae296c10de7bd
Penguino <heriberto@melo.nyc>
2018-03-21 13:24:02 -0400

    Added recipes page, added routes to edit, delete, and show recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,5 +4,33 @@ class RecipeController < ApplicationController
       erb :'/recipes/index'
   end
 
+  get '/recipes/new' do
+    if logged_in?
+      @user = User.find(session[:user_id])
+      erb :'/recipes/new'
+    else
+      flash[:failure] = "Please login to create a new recipe"
+      redirect to '/login'
+    end
+  end
+
+  post '/recipes' do
+    if params[:title].length < 3 || params[:ingredients].empty? || params[:instructions].empty?
+      flash[:failure] = "Please do not leave any entries empty"
+      redirect to '/recipes/new'
+    else
+      @recipe = Recipe.create(title: params[:title], ingredients: params[:ingredients], instructions: params[:instructions], user_id: session[:user_id])
+      redirect to "recipes/#{@recipe.recipe_slug}"
+    end
+  end
+
+  get '/recipes/:slug' do
+    @recipe = Recipe.find_by_recipe_slug(params[:slug])
+    if (session[:user_id] == @recipe.user_id) && @recipe.user_id != nil
+      erb :'/recipes/edit_delete'
+    else
+      erb :'/recipes/show'
+    end
+  end
 
 end
--- app/models/helpers/slugifiable.rb
+++ app/models/helpers/slugifiable.rb
@@ -3,11 +3,18 @@ module Helpers
     def slug
       username.parameterize
     end
+    def recipe_slug
+      title.parameterize
+    end
   end
 
   module Findable
     def find_by_slug(slug)
       self.all.find{|item| item.slug == slug}
+    end
+
+    def find_by_recipe_slug(slug)
+      self.all.find{|item| item.recipe_slug == slug}
     end
   end
 end
